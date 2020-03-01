d60294b085fb8da7a52af6384db93621a041da15
rohan <rohan6471@gmail.com>
2020-03-01 13:29:12 -0600

    created onCreateOptionsMenu stub

--- MediTrans/app/src/main/java/com/example/meditrans/Customer.java
+++ MediTrans/app/src/main/java/com/example/meditrans/Customer.java
@@ -1,5 +1,42 @@
 package com.example.meditrans;
 
 
-public class Customer {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ProgressBar;
+import android.widget.SearchView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer  extends AppCompatActivity implements SearchView.OnQueryTextListener {
+    int id;
+    RecyclerView recyclerView;
+    recyclerAdapter madapter;
+    ProgressBar progressBar;
+    List<Bean> data = new ArrayList<>();
+    SearchView editsearch;
+    SharedPreferences sharedPreferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer);
+        progressBar=findViewById(R.id.progress);
+        sharedPreferences = getSharedPreferences("pref",MODE_PRIVATE);
+        editsearch = (SearchView) findViewById(R.id.search);
+        editsearch.setOnQueryTextListener(this);
+        recyclerView = findViewById(R.id.recycler);
+        getfeedback();
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_dots, menu);
+        return true;
+    }
 }
