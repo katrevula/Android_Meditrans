6aafc580a6434e5039fe3d9eff306e76fe11cf21
kavyareddy536886 <54380608+kavyareddy536886@users.noreply.github.com>
2020-02-29 21:56:37 -0600

    created variables and a contructor in AddReqClass.java class

--- MediTrans/app/src/main/java/com/example/meditrans/AddReqClass.java
+++ MediTrans/app/src/main/java/com/example/meditrans/AddReqClass.java
@@ -1,4 +1,16 @@
 package com.example.meditrans;
 
 public class AddReqClass {
+    private String bname;
+    private String bmobile;
+    private String brequest;
+    private String baddress;
+
+
+    public AddReqClass(String bname, String bmobile, String baddress, String brequest) {
+        this.bname = bname;
+        this.bmobile = bmobile;
+        this.brequest = brequest;
+        this.baddress = baddress;
+    }
 }
