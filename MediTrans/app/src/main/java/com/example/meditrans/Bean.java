0b69210cb2dccd823fc671683c92c27cd434d985
kavyareddy536886 <54380608+kavyareddy536886@users.noreply.github.com>
2020-02-29 22:14:59 -0600

    created a constructor and  declared private variables in Bean.java

--- MediTrans/app/src/main/java/com/example/meditrans/Bean.java
+++ MediTrans/app/src/main/java/com/example/meditrans/Bean.java
@@ -8,4 +8,17 @@ public class Bean {
     public void setId(String id) {
         this.id = id;
     }
+    public Bean(String id) {
+        this.id = id;
+    }
+
+    private String id;
+    private String shopname;
+    private String shopaddress;
+    private String userid;
+    private String password;
+    private String medname;
+    private String medcode;
+    private String medcost;
+    private String meddesc;
 }
