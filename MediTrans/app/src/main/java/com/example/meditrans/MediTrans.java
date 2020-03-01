4cd8908948b08c618908f43f7145ec6263cc7021
Sravani537520 <sravani.jaidi98@gmail.com>
2020-03-01 13:22:06 -0600

    Created onCreate() stub in MediTrans.java

--- MediTrans/app/src/main/java/com/example/meditrans/MediTrans.java
+++ MediTrans/app/src/main/java/com/example/meditrans/MediTrans.java
@@ -1,6 +1,33 @@
 package com.example.meditrans;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MediTrans extends AppCompatActivity {
+    Button b1,b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medi_trans);
+        b1=findViewById(R.id.ba);
+        b2=findViewById(R.id.bb);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Medico.class);
+                startActivity(i);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Customer.class);
+                startActivity(i);
+            }
+        });
+    }
 }
