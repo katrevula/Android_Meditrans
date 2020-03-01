edd389fe4a17ecaedcaea526290554eb9896418d
kavyareddy536886 <54380608+kavyareddy536886@users.noreply.github.com>
2020-02-29 21:43:34 -0600

    overrided onCreatedOptionsMenu() method

--- MediTrans/app/src/main/java/com/example/meditrans/AboutActivity.java
+++ MediTrans/app/src/main/java/com/example/meditrans/AboutActivity.java
@@ -1,6 +1,16 @@
 package com.example.meditrans;
 
+import android.os.Bundle;
+import android.view.Menu;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 }
