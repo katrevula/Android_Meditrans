26b427b3273b864ba87ef593410dbc7762376eb7
Sravani537520 <sravani.jaidi98@gmail.com>
2020-03-01 13:14:45 -0600

    Created onCreate() in Medico.java

--- MediTrans/app/src/main/java/com/example/meditrans/Medico.java
+++ MediTrans/app/src/main/java/com/example/meditrans/Medico.java
@@ -1,8 +1,50 @@
 package com.example.meditrans;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Medico extends AppCompatActivity {
+    EditText e1, e2;
+    Button b1;
+    TextView t1;
+    SharedPreferences sharedPreferences;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medico);
+        sharedPreferences = getSharedPreferences("pref",MODE_PRIVATE);
+        int PERMISSION_ALL = 1;
+        String[] PERMISSIONS = {Manifest.permission.INTERNET,Manifest.permission.CALL_PHONE,Manifest.permission.ACCESS_NETWORK_STATE};
+
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+        e1 = findViewById(R.id.uid);
+        e2 = findViewById(R.id.pass);
+        b1 = findViewById(R.id.bc);
+        t1 = findViewById(R.id.rega);
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MedReg.class);
+                startActivity(intent);
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent a = new Intent(getApplicationContext(), MedHome.class);
+                startActivity(a);
 
 
 
