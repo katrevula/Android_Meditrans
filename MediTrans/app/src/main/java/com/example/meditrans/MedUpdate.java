87105ad6d6432df77b102d78f842bead3585ab53
katrevula <Sandeep.katrevula@gmail.com>
2020-03-01 10:12:10 -0600

    Method Created: Options Menu

--- MediTrans/app/src/main/java/com/example/meditrans/MedUpdate.java
+++ MediTrans/app/src/main/java/com/example/meditrans/MedUpdate.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -51,7 +54,7 @@ public class MedUpdate extends AppCompatActivity {
         rb1 = findViewById(R.id.rbak);
         rb2 = findViewById(R.id.rbbl);
         b1 = findViewById(R.id.bim);
-      
+
 
         awesomeValidation=new AwesomeValidation(ValidationStyle.BASIC);
         awesomeValidation.addValidation(this, R.id.sn,
@@ -78,5 +81,39 @@ public class MedUpdate extends AppCompatActivity {
             }
 
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_dots, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                finish();
+                return true;
+
+            case R.id.shome:
+                Intent i =new Intent(getApplicationContext(),MediTrans.class);
+                startActivity(i);
+                return true;
+            case R.id.about:
+                i =new Intent(getApplicationContext(),AboutActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.contact:
+                i =new Intent(getApplicationContext(),ContactActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
