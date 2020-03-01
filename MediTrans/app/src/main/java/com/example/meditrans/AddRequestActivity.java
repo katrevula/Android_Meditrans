9fb91fd7e7c8df3e211a8416506f448e716cb00f
kavyareddy536886 <54380608+kavyareddy536886@users.noreply.github.com>
2020-02-29 22:08:04 -0600

    overrided onCreateOptionsMenu() in AddRequestActivity.java

--- MediTrans/app/src/main/java/com/example/meditrans/AddRequestActivity.java
+++ MediTrans/app/src/main/java/com/example/meditrans/AddRequestActivity.java
@@ -2,6 +2,8 @@ package com.example.meditrans;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,6 +41,9 @@ public class AddRequestActivity extends AppCompatActivity {
 
             }
         });
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+
     }
     }
-}
+
