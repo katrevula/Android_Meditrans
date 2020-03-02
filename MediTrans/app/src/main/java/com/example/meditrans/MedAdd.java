a3b5202a384c599dce2e6afceffab947d9121f2f
rohan <rohan6471@gmail.com>
2020-03-02 16:38:08 -0600

    updated MedAdd.java

--- MediTrans/app/src/main/java/com/example/meditrans/MedAdd.java
+++ MediTrans/app/src/main/java/com/example/meditrans/MedAdd.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 //import com.example.abhinaya.meditrans.Network.API;
 
@@ -21,14 +22,14 @@ public class MedAdd extends AppCompatActivity {
     RadioGroup rg;
     RadioButton rba,rbb;
     Button b ;
-    SharedPreferences sharedPreferences;
+    //SharedPreferences sharedPreferences;
     String userid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_med_add);
-        sharedPreferences = getSharedPreferences("pref",MODE_PRIVATE);
-        userid = sharedPreferences.getString("USER",null);
+//        sharedPreferences = getSharedPreferences("pref",MODE_PRIVATE);
+//        userid = sharedPreferences.getString("USER",null);
         et1=findViewById(R.id.mename);
         et2=findViewById(R.id.mecode);
         et3=findViewById(R.id.mecost);
@@ -43,27 +44,27 @@ public class MedAdd extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Medicine added", Toast.LENGTH_SHORT).show();
 
 
-
-                String medname = et1.getText().toString();
-                String medcode = et2.getText().toString();
-                String medcost = et3.getText().toString();
-                String medshop=et4.getText().toString();
-                String meddes=et5.getText().toString();
-
-                if(medname.equals("")&&medcode.equals("")&&medcost.equals("")&&medshop.equals("")&&meddes.equals("")){
-                    et1.setError("Not Empty");
-                    et2.setError("Not Empty");
-                    et3.setError("Not Empty");
-                    et4.setError("Not Empty");
-                    et5.setError("Not Empty");
-
-                }else{
-                    int selectedId=rg.getCheckedRadioButtonId();
-                    rba=(RadioButton)findViewById(selectedId);
-                    //            supload();
-                }
+//                String medname = et1.getText().toString();
+//                String medcode = et2.getText().toString();
+//                String medcost = et3.getText().toString();
+//                String medshop=et4.getText().toString();
+//                String meddes=et5.getText().toString();
+//
+//                if(medname.equals("")&&medcode.equals("")&&medcost.equals("")&&medshop.equals("")&&meddes.equals("")){
+//                    et1.setError("Not Empty");
+//                    et2.setError("Not Empty");
+//                    et3.setError("Not Empty");
+//                    et4.setError("Not Empty");
+//                    et5.setError("Not Empty");
+//
+//                }else{
+//                    int selectedId=rg.getCheckedRadioButtonId();
+//                    rba=(RadioButton)findViewById(selectedId);
+//                    //            supload();
+//                }
             }
 
 
