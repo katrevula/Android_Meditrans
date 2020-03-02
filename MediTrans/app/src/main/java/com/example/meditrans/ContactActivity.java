7c5549a68bbf922fe777a7b148fe27b64e178227
rohan <rohan6471@gmail.com>
2020-03-01 18:24:44 -0600

    added onOptionsItemSelected stub in ContactActivity.java

--- MediTrans/app/src/main/java/com/example/meditrans/ContactActivity.java
+++ MediTrans/app/src/main/java/com/example/meditrans/ContactActivity.java
@@ -1,8 +1,10 @@
 package com.example.meditrans;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,5 +19,31 @@ public class ContactActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_dots, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            case R.id.shome:
+                Intent i =new Intent(getApplicationContext(),MediTrans.class);
+                startActivity(i);
+                return true;
+            case R.id.about:
+                i =new Intent(getApplicationContext(),AboutActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.contact:
+                i =new Intent(getApplicationContext(),ContactActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
