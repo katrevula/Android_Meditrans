f224bd33130de9a58badb10bc6dcd9be1e464b2b
rohan <rohan6471@gmail.com>
2020-04-02 17:53:58 -0500

    adding table row in getfeedback stub in meditrans/MedViewMed.java

--- MediTrans/app/src/main/java/com/example/meditrans/MedViewMed.java
+++ MediTrans/app/src/main/java/com/example/meditrans/MedViewMed.java
@@ -4,6 +4,7 @@ package com.example.meditrans;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class MedViewMed extends AppCompatActivity {
     TableLayout tl1;
@@ -15,5 +16,10 @@ public class MedViewMed extends AppCompatActivity {
         setContentView(R.layout.activity_med_view_med);
         tl1 = findViewById(R.id.viewmed);
         getfeedback();
+    }
+    public void getfeedback() {
+        TableRow row = new TableRow(getApplicationContext());
+        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+        row.setLayoutParams(lp);
 
 }
