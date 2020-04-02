96ac6bc525974df68c12b3d546fc485b0ef50a81
rohan <rohan6471@gmail.com>
2020-04-02 17:31:51 -0500

    creating MyViewHolder class

--- MediTrans/app/src/main/java/com/example/meditrans/MedViewReq.java
+++ MediTrans/app/src/main/java/com/example/meditrans/MedViewReq.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -56,6 +58,11 @@ public class MedViewReq extends AppCompatActivity {
         @Override
         public int getItemCount() {
             return data.size();
-        }
 
-    }
+        }
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            TextView sname, smobile, addres, sdesript;
+            ImageView scall;
+
+
+        }
