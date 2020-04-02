10cf6fac467d9dd648f56cb14480d16b866f4f56
rohan <rohan6471@gmail.com>
2020-04-02 17:34:17 -0500

    overriding onCreateOptionsMenu stub

--- MediTrans/app/src/main/java/com/example/meditrans/MedViewReq.java
+++ MediTrans/app/src/main/java/com/example/meditrans/MedViewReq.java
@@ -6,6 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -63,6 +65,21 @@ public class MedViewReq extends AppCompatActivity {
         public class MyViewHolder extends RecyclerView.ViewHolder {
             TextView sname, smobile, addres, sdesript;
             ImageView scall;
-
+            public MyViewHolder(View itemView) {
+                super(itemView);
+                sname = itemView.findViewById(R.id.name);
+                smobile = itemView.findViewById(R.id.phnumber);
+                sdesript = itemView.findViewById(R.id.descript);
+                addres = itemView.findViewById(R.id.address);
+//                scall = itemView.findViewById(R.id.called);
+            }
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_dots, menu);
+        return true;
+    }
 
         }
