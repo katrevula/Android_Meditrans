06b0ca1dd94a99dca2600e53e6a742f29949a44e
rohan <rohan6471@gmail.com>
2020-04-02 16:48:21 -0500

    added onCreate() stub in MedViewReq.java

--- MediTrans/app/src/main/java/com/example/meditrans/MedViewReq.java
+++ MediTrans/app/src/main/java/com/example/meditrans/MedViewReq.java
@@ -1,8 +1,13 @@
 package com.example.meditrans;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 
 
 public class MedViewReq extends AppCompatActivity {
-}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
