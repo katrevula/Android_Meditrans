7f48fb5d869703fbbef677cfc26c97ea940645bf
rohan <rohan6471@gmail.com>
2020-04-02 17:45:42 -0500

    setting view in meditrans/MedViewMed.java

--- MediTrans/app/src/main/java/com/example/meditrans/MedViewMed.java
+++ MediTrans/app/src/main/java/com/example/meditrans/MedViewMed.java
@@ -9,5 +9,6 @@ public class MedViewMed extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_med_view_med);
 
 }
