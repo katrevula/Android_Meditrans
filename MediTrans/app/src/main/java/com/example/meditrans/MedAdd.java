bcc94624e31ac5c8adefc3e8a5d0b6160d6b4cdc
kavyareddy536886 <54380608+kavyareddy536886@users.noreply.github.com>
2020-04-02 17:12:36 -0500

    updated using try in shareApplication() in MedApp.java

--- MediTrans/app/src/main/java/com/example/meditrans/MedAdd.java
+++ MediTrans/app/src/main/java/com/example/meditrans/MedAdd.java
@@ -184,3 +184,18 @@ public class MedAdd extends AppCompatActivity {
 
         // Append file and send Intent
         File originalApk = new File(filePath);
+        try {
+            //Make new directory in new location
+            File tempFile = new File(getExternalCacheDir() + "/ExtractedApk");
+            //If directory doesn't exists create new
+            if (!tempFile.isDirectory())
+                if (!tempFile.mkdirs())
+                    return;
+            //Get application's name and convert to lowercase
+            tempFile = new File(tempFile.getPath() + "/" + getString(app.labelRes).replace(" ","").toLowerCase() + ".apk");
+            //If file doesn't exists create new
+            if (!tempFile.exists()) {
+                if (!tempFile.createNewFile()) {
+                    return;
+                }
+            }
