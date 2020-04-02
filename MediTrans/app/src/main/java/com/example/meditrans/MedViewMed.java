2e6e622508b61b5273a945dc95d05310f5628226
rohan <rohan6471@gmail.com>
2020-04-02 17:57:58 -0500

    overriding onCreateOptionsMenu in meditrans/MedViewMed.java

--- MediTrans/app/src/main/java/com/example/meditrans/MedViewMed.java
+++ MediTrans/app/src/main/java/com/example/meditrans/MedViewMed.java
@@ -4,6 +4,8 @@ package com.example.meditrans;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -57,4 +59,10 @@ public class MedViewMed extends AppCompatActivity {
         tl1.addView(row);
 
 }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_dots, menu);
+        return true;
+    }
 }
