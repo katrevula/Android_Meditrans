21b6180d12937732af0577237dd279e2b3baf64c
kavyareddy536886 <54380608+kavyareddy536886@users.noreply.github.com>
2020-04-02 16:38:17 -0500

    update

--- MediTrans/app/src/main/java/com/example/meditrans/MedViewReq.java
+++ MediTrans/app/src/main/java/com/example/meditrans/MedViewReq.java
@@ -2,10 +2,7 @@ package com.example.meditrans;
 
 import android.support.v7.app.AppCompatActivity;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 06f3b6ecb5956f0ece0cdec717d06c57b97956a7
 
 public class MedViewReq extends AppCompatActivity {
 }
