6afc6fa6328ecb1a0fa5e8ea1f4520baf033b89d
katrevula <Sandeep.katrevula@gmail.com>
2020-04-02 17:13:18 -0500

    Update MedicineDetails.java

--- MediTrans/app/src/main/java/com/example/meditrans/MedicineDetails.java
+++ MediTrans/app/src/main/java/com/example/meditrans/MedicineDetails.java
@@ -65,6 +65,7 @@ public class MedicineDetails extends AppCompatActivity  {
         jobsBean1.setMedname("dolo");
         jobsBean1.setMedcost("90");
         jobsBean1.setMeddesc("fever");
+        data.add(jobsBean1);
     }
     private class MedicineAdapter extends RecyclerView.Adapter<MedicineDetails.MedicineAdapter.MyViewHolder> {
         List<Bean> horizontalList;
