22ce0e38de31cf0d942bfc9b65691907d966fd37
kavyareddy536886 <54380608+kavyareddy536886@users.noreply.github.com>
2020-04-16 23:30:28 -0500

    added getter and setter method for password in signup class

--- MediTrans/app/src/main/java/com/example/meditrans/SignupDetails.java
+++ MediTrans/app/src/main/java/com/example/meditrans/SignupDetails.java
@@ -37,5 +37,11 @@ public class SignupDetails {
     public void setUserName(String userName) {
         this.userName = userName;
     }
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
