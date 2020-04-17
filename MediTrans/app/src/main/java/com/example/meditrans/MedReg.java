0fd4103eea05f5f120ff1d81b3bba4076bb2785b
rohan <rohan6471@gmail.com>
2020-04-16 23:40:00 -0500

    Calling sendEmailVerification method in MedReg.java

--- MediTrans/app/src/main/java/com/example/meditrans/MedReg.java
+++ MediTrans/app/src/main/java/com/example/meditrans/MedReg.java
@@ -20,10 +20,13 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -115,7 +118,10 @@ public class MedReg extends AppCompatActivity {
 
                                     FirebaseAuth auth = FirebaseAuth.getInstance();
                                     FirebaseUser user = auth.getCurrentUser();
-
+                                    user.sendEmailVerification()
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
 
                                     Intent a = new Intent(getApplicationContext(), Medico.class);
                 startActivity(a);
