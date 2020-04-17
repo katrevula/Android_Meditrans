ce5af1ddaa7411f56b84d04086bf4f9e0a297e5e
kavyareddy536886 <54380608+kavyareddy536886@users.noreply.github.com>
2020-04-16 23:33:15 -0500

    added getter and setter method for email in signup class

--- MediTrans/app/src/main/java/com/example/meditrans/SignupDetails.java
+++ MediTrans/app/src/main/java/com/example/meditrans/SignupDetails.java
@@ -58,4 +58,12 @@ public class SignupDetails {
     public void setPhone(String phone) {
         this.phone = phone;
     }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
